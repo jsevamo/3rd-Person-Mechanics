0aa68ebf4ad64c59673d40d5c76885b8c88f52cc
jsevamo <j_sevamo@hotmail.com>
2020-06-27 16:05:51 -0500

    Added boxMan with Animation Controller

---
+++ 3rd Person Gameplay/Assets/Scripts/Utilities/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace  SA
+{
+    public class Helper : MonoBehaviour
+    {
+
+        [Range(0, 1)] public float vertical;
+        private Animator anim;
+        
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            anim.SetFloat("Vertical", vertical);
+        }
+    }
+
+}
