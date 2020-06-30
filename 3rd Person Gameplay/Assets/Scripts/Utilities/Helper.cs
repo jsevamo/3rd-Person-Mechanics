372f5cd7fe671df9c1d688064f1890f9a218ab41
jsevamo <j_sevamo@hotmail.com>
2020-06-30 14:47:35 -0500

    Added Slashing Sword Animation Test

--- 3rd Person Gameplay/Assets/Scripts/Utilities/Helper.cs
+++ 3rd Person Gameplay/Assets/Scripts/Utilities/Helper.cs
@@ -10,6 +10,12 @@ namespace  SA
 
         [Range(0, 1)] public float vertical;
         private Animator anim;
+        public bool playAnim;
+
+        public string[] oh_attacks;
+        public string[] th_attacks;
+
+        public bool twoHanded;
         
         void Start()
         {
@@ -19,6 +25,28 @@ namespace  SA
         // Update is called once per frame
         void Update()
         {
+            anim.SetBool("Two_Handed", twoHanded);
+            
+            if (playAnim)
+            {
+                string targetAnim;
+
+                if (!twoHanded)
+                {
+                    int r = Random.Range(0, oh_attacks.Length);
+                    targetAnim = oh_attacks[r];
+                }
+                else
+                {
+                    int r = Random.Range(0, th_attacks.Length);
+                    targetAnim = th_attacks[r];
+                }
+                
+                vertical = 0;
+                anim.CrossFade(targetAnim, 0.6f);
+                playAnim = false;
+            }
+            
             anim.SetFloat("Vertical", vertical);
         }
     }
