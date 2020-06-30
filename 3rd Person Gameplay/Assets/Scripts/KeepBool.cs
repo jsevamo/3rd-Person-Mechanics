c498af673d74914de728834fcd77bdd72c752e29
jsevamo <j_sevamo@hotmail.com>
2020-06-30 15:53:33 -0500

    Applying root motion while attacking

---
+++ 3rd Person Gameplay/Assets/Scripts/KeepBool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepBool : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    public string boolName;
+    public bool status;
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //animator.SetBool(boolName, status);
+        animator.SetBool(boolName, true);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, false);
+        //animator.SetBool(boolName, !status);
+      
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- 3rd Person Gameplay/Assets/Scripts/Utilities/Helper.cs
+++ 3rd Person Gameplay/Assets/Scripts/Utilities/Helper.cs
@@ -11,11 +11,16 @@ namespace  SA
         [Range(0, 1)] public float vertical;
         private Animator anim;
         public bool playAnim;
-
+        
+        // String list of the names of attacks. One Handed and Two Handed
         public string[] oh_attacks;
         public string[] th_attacks;
-
+        
+        //Variable to activate two handed carry
         public bool twoHanded;
+        
+        //See if roor motion is enabled. Only for attacks.
+        public bool enableRootMotion;
         
         void Start()
         {
@@ -25,25 +30,42 @@ namespace  SA
         // Update is called once per frame
         void Update()
         {
-            anim.SetBool("Two_Handed", twoHanded);
+            //CanMove is a variable that lets me know if i can apply root motion
             
+            bool cannotMove = !anim.GetBool("CanMove");
+            enableRootMotion = !anim.GetBool("CanMove");
+            
+            // I want to apply root motion only if I am attacking (Cannot move)
+            anim.applyRootMotion = !anim.GetBool("CanMove");
+            
+            //If I cannot move, I get out of the Update Loop
+            if (cannotMove)
+                return;
+            
+            //Check if we press "Two handed"
+            anim.SetBool("Two_Handed", twoHanded);
+
             if (playAnim)
             {
                 string targetAnim;
-
+                //If not two handed, choose one handed attacks
                 if (!twoHanded)
                 {
                     int r = Random.Range(0, oh_attacks.Length);
                     targetAnim = oh_attacks[r];
                 }
+                //If two handed, choose two handed attacks
                 else
                 {
                     int r = Random.Range(0, th_attacks.Length);
                     targetAnim = th_attacks[r];
                 }
-                
+                //Vertical is the variable to blend walk and running animation.
+                //Since we are attacking, it must be 0.
                 vertical = 0;
-                anim.CrossFade(targetAnim, 0.6f);
+                //we crossfade de selected attacking animation
+                anim.CrossFade(targetAnim, 0.2f);
+                //When we are done, play anim goes false again.
                 playAnim = false;
             }
             
